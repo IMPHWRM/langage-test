7a5e5a6db7b7a1c701746210cefa0700c6cc7f69
IMPHWRM <int.main.return.main@gmail.com>
2024-12-28 13:23:23 +0100

    cool man

--- src/main.c
+++ src/main.c
@@ -1,6 +1,18 @@
-#include <sdtio.h>
+#include <stdio.h>
 #include <stdlib.h>
-int main()
+int main(int argc,char* argv[])
 {
+	if(argc==1)
+	{
+		return 0;
+	}
+	for(int i=0;i<argc;i++)
+	{
+		if(argv[i][0]==45)
+		{
+			if(argv[i][1]==104)
+			{
+				printf("-o   the binary file\n and the rest I will see..\n");
+	}
 	return 0;
 }
